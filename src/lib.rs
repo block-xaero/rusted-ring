a556255c25fcd8953516fcfec0d34ed371b9e814
Anirudh Vyas <ricky.nj@gmail.com>
2025-07-14 08:11:32 -0700

    adds RingPipe construct to be able to communicate back and forth to 2 ringbuffers serving as input and out of a chain of pipeline

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,51 @@ pub mod ring;
 
 pub use pool::*;
 pub use ring::*;
+
+/// A Ring Source is the emitter bound to ring buffer
+/// Essentially has a writer that is used by underlying `user` - lets say an actor
+/// This can be leveraged to write to source which then can be read by others.
+/// RingBuffer is a shared entity, expected to be static,
+/// However it is expected that we follow Single Writer and many readers principle,
+/// Source emits events (See `PooledEvent<TSHIRT_SIZE>` for more details).
+#[derive(Debug)]
+pub struct RingSource<const TSHIRT_SIZE: usize, const RING_CAPACITY: usize> {
+    writer: Writer<TSHIRT_SIZE, RING_CAPACITY>,
+}
+impl<const TSHIRT_SIZE: usize, const RING_CAPACITY: usize> RingSource<TSHIRT_SIZE, RING_CAPACITY> {
+    pub fn new(out_buffer: &'static RingBuffer<TSHIRT_SIZE, RING_CAPACITY>) -> Self {
+        RingSource {
+            writer: Writer::new(out_buffer),
+        }
+    }
+}
+
+/// A Ring Sink is the just that: a Sink that is  bound to ring buffer
+/// Why a reader? Sink carries an in_buffer which `outsider` writes to.
+/// Reader then used by receiving actor to process messages.
+#[derive(Debug, Clone)]
+pub struct RingSink<const TSHIRT_SIZE: usize, const RING_CAPACITY: usize> {
+    reader: Reader<TSHIRT_SIZE, RING_CAPACITY>,
+}
+
+impl<const TSHIRT_SIZE: usize, const RING_CAPACITY: usize> RingSink<TSHIRT_SIZE, RING_CAPACITY> {
+    pub fn new(in_buffer: &'static RingBuffer<TSHIRT_SIZE, RING_CAPACITY>) -> Self {
+        RingSink {
+            reader: Reader::new(in_buffer),
+        }
+    }
+}
+
+/// A conduit meant as a channel replacement in a stack friendly way
+/// This enables actors to communicate following - Single Write and Many reads principle.
+/// Source and Sink both which are underlying entities of a RingPipe
+/// are essentially mechanisms for external parties communicating with anyone
+/// using a RingPipe to leverage reader and writers.
+/// NOTE: the IN_BUFFER is ringbuffer that writer writes to and is read by entity
+/// using the RingPipe using `RingSink`'s `Reader`.
+/// Like wise Writer is used in a manner similar.
+#[derive(Debug)]
+pub struct RingPipe<const TSHIRT_SIZE: usize, const RING_CAPACITY: usize> {
+    source: RingSource<TSHIRT_SIZE, RING_CAPACITY>,
+    sink: RingSink<TSHIRT_SIZE, RING_CAPACITY>,
+}
